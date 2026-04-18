1de67a37a6886b9083f74f2cf867b040f490a851
Shruti Singh Vishene <shrutivishene@gmail.com>
2026-04-18 13:34:25 +0530

    fix cors + express v5 issue

--- backend/connection.js
+++ backend/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/expense-tracker");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (err) {
         console.log("Failed to connect", err);
